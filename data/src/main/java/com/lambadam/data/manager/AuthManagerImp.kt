329032fc486dc3dab5aa6ea8235c3d12ac62a1fb
tuna bolat <bolattuna@gmail.com>
2018-10-10 00:20:34 +0300

    Added facebook-api,firebase-auth to gradle

--- data/src/main/java/com/lambadam/data/manager/AuthManagerImp.kt
+++ data/src/main/java/com/lambadam/data/manager/AuthManagerImp.kt
@@ -1,3 +1,23 @@
 package com.lambadam.data.manager
 
-class AuthManagerImp : AuthMan
+import com.lambadam.domain.auth.AuthManager
+import com.lambadam.domain.auth.AuthType
+import com.lambadam.domain.model.None
+import com.lambadam.domain.model.Result
+import com.lambadam.domain.model.User
+
+class AuthManagerImp : AuthManager {
+
+    override fun getCurrentUser(): Result<Exception, User> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun logout(): Result<Exception, None> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun login(type: AuthType, token: String): Result<Exception, None> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
