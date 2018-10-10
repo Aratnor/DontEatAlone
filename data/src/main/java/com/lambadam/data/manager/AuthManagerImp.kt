b11d540f27e6f907d0ded4ef5e47b36fdd793994
tuna bolat <bolattuna@gmail.com>
2018-10-10 13:22:59 +0300

    Added getCurrentUser ,logout function to AutMAnagerImp

--- data/src/main/java/com/lambadam/data/manager/AuthManagerImp.kt
+++ data/src/main/java/com/lambadam/data/manager/AuthManagerImp.kt
@@ -1,5 +1,7 @@
 package com.lambadam.data.manager
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.lambadam.domain.auth.AuthManager
 import com.lambadam.domain.auth.AuthType
 import com.lambadam.domain.model.None
@@ -8,16 +10,46 @@ import com.lambadam.domain.model.User
 
 class AuthManagerImp : AuthManager {
 
+    private lateinit var mAuth: FirebaseAuth
+
+
+
     override fun getCurrentUser(): Result<Exception, User> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+
+        mAuth = FirebaseAuth.getInstance()
+
+        var currentUser = mAuth.currentUser
+
+        if(currentUser == null)
+            return Result.buildError(Exception("No user logged in!"))
+        else {
+            var user = User(currentUser.uid,"", currentUser.displayName!!, currentUser.displayName!!, currentUser.email!!,currentUser.photoUrl.toString())
+
+            return Result.buildValue {  user }
+
+        }
+
+
+     }
 
     override fun logout(): Result<Exception, None> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        mAuth = FirebaseAuth.getInstance()
+
+        try {
+            mAuth.signOut()
+        }
+        catch(e: FirebaseAuthInvalidUserException) {
+            return Result.buildError(e);
+        }
+
+           return Result.buildValue {
+               None()
+           }
     }
 
     override fun login(type: AuthType, token: String): Result<Exception, None> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
--- domain/src/main/java/com/lambadam/domain/model/User.kt
+++ domain/src/main/java/com/lambadam/domain/model/User.kt
@@ -1,5 +1,8 @@
 package com.lambadam.domain.model
 
+import com.sun.jndi.toolkit.url.Uri
+import java.net.URI
+
 data class User(val id: String,
                 val userName: String,
                 val name: String,
