8cfe3be468dec54bf1c8b27c354a53f6d9f6cd4e
Aratnor <bolattuna@gmail.com>
2018-10-14 16:53:17 +0300

    Added Google_Login Function

--- app/src/main/java/com/lambadam/donteatalone/login/LoginActivity.kt
+++ app/src/main/java/com/lambadam/donteatalone/login/LoginActivity.kt
@@ -14,9 +14,18 @@ import com.facebook.FacebookException
 import com.facebook.FacebookSdk
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.SignInButton
 import com.lambadam.domain.auth.AuthType
 import com.lambadam.donteatalone.R
 import com.lambadam.donteatalone.post.PostActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -31,8 +40,11 @@ class LoginActivity : AppCompatActivity() {
         var TAG = "Facebook Login"
         val mCallbackManager : CallbackManager
 
+        val mGoogleSignInClient : GoogleSignInClient
 
-        val login_button = findViewById<LoginButton>(R.id.login_button);
+        val login_button = findViewById<LoginButton>(R.id.login_button)
+
+        val signInButton = findViewById<SignInButton>(R.id.signInButton)
 
         viewModel.login.observe(this, Observer { navigateToPostActivity() })
 
@@ -55,7 +67,51 @@ class LoginActivity : AppCompatActivity() {
                 Log.d(TAG, "facebook:onError", error)
                 // ...
             }
-        });
+        })
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        signInButton.setOnClickListener{
+            Log.i("Google","Google Login Clicked")
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+            /*val task = mGoogleSignInClient.silentSignIn()
+            if (task.isSuccessful) {
+                // There's immediate result available.
+                val signInAccount = task.result!!
+                login(AuthType.GOOGLE, signInAccount.idToken!!)
+            }
+            else {
+                task.addOnCompleteListener {
+                        try {
+                            task.getResult(ApiException::class.java)
+                        } catch (apiException: ApiException) {
+                            throw apiException
+                        }
+
+                }
+            }*/
+        }
+    }
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebas
+                val account = task.getResult(ApiException::class.java)!!
+                login(AuthType.GOOGLE,account.idToken!!)
+            }
+            catch (e: ApiException) {
+                Log.e("Error Google Login",e.toString());
+            }
+        }
+
+
     }
 
     private fun navigateToPostActivity() {
@@ -64,5 +120,9 @@ class LoginActivity : AppCompatActivity() {
 
     fun login(authType: AuthType,token: String) {
         viewModel.login(authType,token)
+    }
+
+    companion object {
+        private val RC_SIGN_IN = 9001
     }
 }
